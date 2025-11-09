65e0bdad78320c937a564b5483fb56a1b6a47f01
Shruti V S <35287339+shrutivs88@users.noreply.github.com>
2025-11-09 18:20:41 +0530

    Enable CORS and set security headers in Vite config
    Modified in vite.config file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+     cors:true,
+    headers: {
+      "Cross-Origin-Opener-Policy": "unsafe-none",
+      "Cross-Origin-Embedder-Policy": "unsafe-none"
+    }
+   }
 })
+
